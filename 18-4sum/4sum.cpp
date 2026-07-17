7e0d9a2894cd35c3079d053da1f680b1a980a14f
Satyam Kumar Jha <0satyamkumarjha2@gmail.com>
2026-07-17 14:15:05 +0530

    Time: 22 ms (60.23%) | Memory: 17.3 MB (84.64%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
+        vector<vector<int>>satyam;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
 
-        for(int i=0; i<n-1; i++) {
-            if(i>0 && nums[i]==nums[i- 1])
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n-1; i++){
+            if(i > 0 && nums[i] == nums[i - 1])
                 continue;
-            for(int j =i +1; j<n-2; j++) {
-                if(j>i + 1 && nums[j]==nums[j-1])
-                    continue;
+        for(int j = i + 1; j < n-1; j++){
+            if(j > i+1 && nums[j] == nums[j-1])
+                continue;
 
-                int l = j+1;
-                int r = n-1;
+        int left = j+1;
+        int right = n-1;
 
-                while(l<r) {
-                long long total = (long long)nums[i] +nums[j]+nums[l]+nums[r];
-                    if(total == target) {
-                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
-                        l++;
-                        r--;
+        while(left<right){
+            long long total = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+            if(total == target){
+                satyam.push_back({nums[i], nums[j], nums[left], nums[right]});
+                while(left < right && nums[left] == nums [left+1]) left++;
+                while(left < right && nums[right] == nums [right-1]) right--;
 
-                        while(l<r && nums[l]==nums[l-1])
-                            l++;
-
-                        while(l<r && nums[r]==nums[r+1])
-                            r--;
-                    }
-                    else if(total<target) {
-                        l++;
-                    }
-                    else {
-                        r--;
-                    }
-                }
+                left++;
+                right--;
+            }
+            else if( total < target)
+                left++;
+            else
+                right--;  
             }
         }
-        return ans;
+    }
+    return satyam;      
     }
 };
