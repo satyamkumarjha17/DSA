b2f6cd5fc0674c055308c8cf4b398152a858a346
Satyam Kumar Jha <0satyamkumarjha2@gmail.com>
2026-07-24 11:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.30%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,22 @@
 class Solution {
-public:
-    int lengthOfLastWord(string s) {
+private:
+    int lengthOfLastWordHelper(string s) {
         int n = s.size();
         int len = 0;
 
         int i = n - 1;
-        while(i >= 0 && s[i] == ' ')
+        while (i >= 0 && s[i] == ' ')
             i--;
 
-        while(i >= 0 && s[i] != ' ') {
+        while (i >= 0 && s[i] != ' ') {
             len++;
             i--;
         }
 
         return len;
+    }
+public:
+    int lengthOfLastWord (string s) {
+        return lengthOfLastWordHelper(s);
     }
 };
