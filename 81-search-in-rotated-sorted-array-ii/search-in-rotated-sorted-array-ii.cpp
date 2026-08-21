2d37f7102b1bb1e5413637bb837922886fd5263d
Satyam Kumar Jha <0satyamkumarjha2@gmail.com>
2026-08-21 23:17:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (46.61%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target){
         int left = 0;
         int right = nums.size() - 1;
 
