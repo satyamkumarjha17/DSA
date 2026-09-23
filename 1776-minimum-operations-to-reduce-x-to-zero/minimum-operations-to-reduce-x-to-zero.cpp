74df4e44ed60fd86ff630624567918970a30f7cc
Satyam Kumar Jha <0satyamkumarjha2@gmail.com>
2026-09-23 11:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 102.3 MB (91.82%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int target = totalSum - x;
+
+        if (target < 0) {
+            return -1;
+        }
+
+        if (target == 0) {
+            return n;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (sum > target && left <= right) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) {
+            return -1;
+        }
+
+        return n - maxLen;
+    }
+};
