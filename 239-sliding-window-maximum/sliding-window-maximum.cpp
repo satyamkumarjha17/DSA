187cd165346212c8fec355264a849aa25ca195f1
Satyam Kumar Jha <0satyamkumarjha2@gmail.com>
2026-09-25 10:51:43 +0530

    Time: 17 ms (79.99%) | Memory: 133.7 MB (99.76%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,21 +1,51 @@
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         deque<int> dq;
+//         vector<int> ans;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             while (!dq.empty() && dq.front() <= i - k)
+//                 dq.pop_front();
+
+//             while (!dq.empty() && nums[dq.back()] <= nums[i])
+//                 dq.pop_back();
+
+//             dq.push_back(i);
+
+//             if (i >= k - 1)
+//                 ans.push_back(nums[dq.front()]);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> ans;
+        int n = nums.size();
+        vector<int> ans(n - k + 1);
 
-        for (int i = 0; i < nums.size(); i++) {
-            while (!dq.empty() && dq.front() <= i - k)
-                dq.pop_front();
+        int* index = new int[n];
+        int left = 0;
+        int right = -1;
 
-            while (!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
+        for (int i = 0; i < n; i++) {
 
-            dq.push_back(i);
+            while (left <= right && index[left] <= i - k)
+                left++;
+
+            while (left <= right && nums[index[right]] <= nums[i])
+                right--;
+
+            index[++right] = i;
 
             if (i >= k - 1)
-                ans.push_back(nums[dq.front()]);
+                ans[i - k + 1] = nums[index[left]];
         }
+
+        delete[] index;
 
         return ans;
     }
